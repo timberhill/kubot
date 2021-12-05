7ffa15d4cc163558bf765c2e07f8f1c4a8d1ca6f
Maksym Lisogorskyi <m.lisogorskyi@gmail.com>
2021-12-05 20:19:02 +0000

    core-1 added version handling and praw dependency

--- dispatcher/kubot_dispatcher/__init__.py
+++ dispatcher/kubot_dispatcher/__init__.py
@@ -1,8 +1,25 @@
 from .config import Config
-from .exceptions import KubotDispatcherConfigError, KubotDispatcherError
+from .dispatcher import Dispatcher
+from .exceptions import (
+    KubotDispatcherConfigError,
+    KubotDispatcherError,
+    KubotDispatcherRedditError
+)
 
 __all__ = [
     "Config",
+    "Dispatcher",
     "KubotDispatcherError",
-    "KubotDispatcherConfigError"
+    "KubotDispatcherConfigError",
+    "KubotDispatcherRedditError"
 ]
+
+# get package version
+from pathlib import Path
+import re
+
+pyproject_path = Path(__spec__.origin).parent.parent / "pyproject.toml"
+with open(pyproject_path, "r") as pyproject_file:
+    contents = pyproject_file.read()
+    version_match = re.search(r"version = \"(?P<value>.+)\"\n", contents)
+    __version__ = version_match.group(1)
--- dispatcher/kubot_dispatcher/__main__.py
+++ dispatcher/kubot_dispatcher/__main__.py
@@ -1,4 +1,17 @@
 from .config import Config
+from .dispatcher import Dispatcher
+from . import __version__
+
 
 config = Config.from_file("config-example.yaml")
-print(config.bots)
+api_config = dict(
+    client_id="CLIENT_ID",
+    client_secret="CLIENT_SECRET",
+    password="PASSWORD",
+    user_agent=f"python:{__package__}:{__version__}",
+    username="USERNAME",
+)
+
+print(api_config)
+
+dispatcher = Dispatcher(config, api_config)

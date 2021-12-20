5ff5bd72f488640e432d1589a084343336d19d78
Maksym Lisogorskyi <m.lisogorskyi@gmail.com>
2021-12-20 09:33:50 +0000

    core-4 fixed tests

--- package/tests/dispatcher/test_dispatcher.py
+++ package/tests/dispatcher/test_dispatcher.py
@@ -4,5 +4,5 @@
 def test_import() -> None:
     """Test dispatcher class import
     """
-    from kubot.dispatcher import Dispatcher
-    assert type(Dispatcher) == type(type)
+    from kubot.dispatcher import KubotDispatcher
+    assert type(KubotDispatcher) == type(type)
